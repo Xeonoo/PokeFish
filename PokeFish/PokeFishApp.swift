43b2c57abcb0241fff7854ff0c604de52201064c
Maksymilian <ts3joker@gmail.com>
2022-07-06 14:28:10 +0200

    Initial Commit

---
+++ PokeFish/PokeFishApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeFishApp.swift
+//  PokeFish
+//
+//  Created by Maksymilian Patoń on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeFishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
