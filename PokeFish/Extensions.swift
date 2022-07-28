83fc4ec6fe1661703477df019b8e46984549bccd
Maksymilian <ts3joker@gmail.com>
2022-07-28 15:35:52 +0200

    weather /5

--- PokeFish/Extensions.swift
+++ PokeFish/Extensions.swift
@@ -9,6 +9,6 @@ import Foundation
 
 extension  Double {
     func roundDouble() -> String {
-        return String(format: "%.0f", self )
+        return String(format: "%.0f", self)
     }
 }
--- PokeFish/Views/WeatherMainView.swift
+++ PokeFish/Views/WeatherMainView.swift
@@ -1,24 +1,45 @@
+////
+////  WeatherMainView.swift
+////  PokeFish
+////
+////  Created by Maksymilian Patoń on 28/07/2022.
+////
 //
-//  WeatherMainView.swift
-//  PokeFish
+//import SwiftUI
 //
-//  Created by Maksymilian Patoń on 28/07/2022.
+//struct WeatherMainView: View {
+//    var weather: ResponseBody
 //
-
-import SwiftUI
-
-struct WeatherMainView: View {
-    var weather: ResponseBody
-     
-    var body: some View {
-        ZStack(alignment: .leading) {
-            
-        }
-    }
-}
-
-struct WeatherMainView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeatherMainView(weather: previewWeather)
-    }
-}
+//    var body: some View {
+//        ZStack(alignment: .leading) {
+//            VStack {
+//                VStack(alignment: .leading, spacing: 5) {
+//                    Text(weather.name)
+//                        .bold().font(.title)
+//                    Text("Today, \(Date().formatted(.dateTime.month().day().hour().minute()))")
+//                        .fontWeight(.light)
+//                }
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//
+//                Spacer()
+//
+//                VStack {
+//                    HStack {
+//
+//                    }
+//                }
+//                .frame(maxWidth: .infinity)
+//            }
+//            .padding()
+//            .frame(maxWidth: .infinity, alignment: .leading)
+//        }
+//        .edgesIgnoringSafeArea(.bottom)
+//        .background(Color.green)
+//    }
+//}
+//
+//struct WeatherMainView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        WeatherMainView(weather: previewWeather)
+//    }
+//}
